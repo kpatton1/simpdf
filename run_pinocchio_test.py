ac3787251ddbc87ea969d2e4b43b0ec915ccf52f
Kenneth Patton <kpatton1@gmail.com>
2014-11-01 15:07:31 -0400

    Standard diff cosmological models

--- run_pinocchio_test.py
+++ run_pinocchio_test.py
@@ -2,7 +2,7 @@
 import os
 from pysimpdf.core import Simulation
 
-f = ['f', 0.0, 1, 1]
+f = ['f', 0.0, 20, 5]
 
 t = [['h', 0.76, 5, 5],
      ['ok', 0.10, 5, 5],
@@ -10,16 +10,27 @@ t = [['h', 0.76, 5, 5],
      ['s8', 0.90, 5, 5],
      ['w', -0.90, 5, 5]]
 
+#l = [f] + t
+
+l = t + [f]
+
+print l
+
 maps={4096:(4096,384,110,(-1.0e15,10.0e15))}
 nside=8192
 
 basedir='data'
 
-s = Simulation(basedir,f,nside,maps)
+ls = []
 
-s.run_pinocchio()
+for i in l:
+    ls.append(Simulation(basedir,i,nside,maps))
 
-s.convert_healpix()
 
-s.add_noise()
+for s in ls:
+    s.run_pinocchio()
+
+for s in ls:
+    s.convert_healpix()
+    s.add_noise()
 
