af4a224479520828f96050340777352a356c1665
Kenneth Patton <kpatton1@gmail.com>
2015-05-11 23:08:23 -0400

    plotting routines

---
+++ plot_fisher2_real.py
@@ -0,0 +1,115 @@
+
+
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from matplotlib.patches import Ellipse
+
+#fisher = 'data/F_8192_fullcov_0.npz'
+#basetitle = 'PS (nside 256)'
+
+fisher = 'data_real/F_1024_test_pdf256_3.npz'
+basetitle = 'PDF (nside 256)'
+
+#fisher = 'data/F_8192_fullcov_35.npz'
+#basetitle = 'PDF (nside 256+1024)'
+
+data = numpy.load(fisher)
+
+F = data['F']
+F_inv = data['F_inv']
+params = data['params']
+
+n = len(params)
+
+names = params
+
+centers = {'h':0.69,'ok':0.0, 'w':-1.0, 's8':0.82, 'om':0.29, 'q':1.0}
+
+print F
+
+F_nog = F[:-1,:-1]
+
+F_noq = F[:-2,:-2]
+
+F_priorq = F_nog.copy()
+F_priorq[-1,-1] += 1/(0.1*0.1)
+
+for p in params:
+    print p
+
+def plot_fisher(F, basetitle, i,j, color, rescale=False):
+    
+    F_inv = numpy.linalg.inv(F)
+    
+    ixgrid = numpy.ix_([j,i],[j,i])
+    
+    subM = F_inv[ixgrid]
+    
+    print subM
+    
+    sigma1 = numpy.sqrt(subM[0,0])
+    sigma2 = numpy.sqrt(subM[1,1])
+    
+    subF = numpy.linalg.inv(subM)
+    
+    l, v = numpy.linalg.eig(subM)
+    l = numpy.sqrt(l)
+    
+    area = l[0]*l[1]*numpy.pi
+    
+    print names[j] + ' sd: ' + str(sigma1)
+    print names[i] + ' sd: ' + str(sigma2)
+    print 'area: ' + str(area)
+    print 'fom: ' + str(1.0/area)
+    
+    ax = plt.subplot(111)
+    ax.set_title(basetitle + ': ' + names[j] + ' vs ' + names[i])
+    
+    ax.set_xlabel(names[j])
+    ax.set_ylabel(names[i])
+    
+    center_i = centers[params[i]]
+    center_j = centers[params[j]]
+    
+    ell = Ellipse(xy=(center_j,center_i),width=l[0]*2, height=l[1]*2, angle=numpy.rad2deg(-numpy.arccos(v[0,0])))
+    ell.set_facecolor('none')
+    ell.set_edgecolor(color)
+    ax.add_artist(ell)
+    
+    if rescale:
+        plt.xlim([center_j-numpy.sqrt(subM[0,0])*1.2,center_j+numpy.sqrt(subM[0,0])*1.2])
+        plt.ylim([center_i-numpy.sqrt(subM[1,1])*1.2,center_i+numpy.sqrt(subM[1,1])*1.2])
+        plt.scatter([center_j],[center_i])
+
+
+for i in range(n):
+    for j in range(i):
+
+        if names[i] != 's8' and names[i] != 'q':
+            continue
+        if names[j] != 'om' and names[j] != 's8':
+            continue
+
+        handles = []
+
+        plot_fisher(F_nog, basetitle, i,j,'blue',True)
+
+        plt.plot([],color='blue',label='Marginalize over q')
+
+        plot_fisher(F_priorq, basetitle, i,j,'green',False)
+
+        plt.plot([],color='green',label='Prior on q')
+
+        if i < n-2 and j < n-2:
+
+            plot_fisher(F_noq, basetitle, i,j,'red', False)
+            
+            plt.plot([],color='red',label='Exactly known q')
+        
+        plt.legend()
+
+        #plt.xlim([0.17,0.42])
+        #plt.ylim([0.70,0.92])
+
+        plt.show()
---
+++ plot_fisher3_real.py
@@ -0,0 +1,185 @@
+
+
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from matplotlib.patches import Ellipse
+
+fisher = 'data_real/F_1024_test_ps_0.npz'
+basetitle = 'PS vs PDF'
+
+fisher2 = 'data_real/F_1024_test_simple_3.npz'
+
+fisher3 = 'data_real/F_1024_test_pdf256_3.npz'
+
+fisher4 = 'data_real/F_1024_test_simple_7.npz'
+
+fisher5 = 'data/F_8192_fullcovmoments_05.npz'
+
+fisher6 = 'data/F_8192_fullcovmoments_12.npz'
+
+fisher7 = 'data/F_8192_fullcovmoments_10.npz'
+
+
+#fisher = 'data/F_8192_fullcov_7.npz'
+#basetitle = 'PDF (nside 1024)'
+
+#fisher = 'data/F_8192_fullcov_3.npz'
+#basetitle = 'PDF (nside 4096)'
+
+data = numpy.load(fisher)
+F = data['F']
+params = data['params']
+
+data = numpy.load(fisher2)
+F2 = data['F']
+
+data = numpy.load(fisher3)
+F3 = data['F']
+
+data = numpy.load(fisher4)
+F4 = data['F']
+
+data = numpy.load(fisher5)
+F5 = data['F']
+
+data = numpy.load(fisher6)
+F6 = data['F']
+
+data = numpy.load(fisher7)
+F7 = data['F']
+
+n = len(params)
+
+names = params
+
+print names
+
+centers = {'h':0.69,'ok':0.0, 'w':-1.0, 's8':0.82, 'om':0.29, 'q':1.0}
+
+#print F4
+
+F[-2,-2] += 1/(0.1*0.1)
+F2[-2,-2] += 1/(0.1*0.1)
+F3[-2,-2] += 1/(0.1*0.1)
+F4[-2,-2] += 1/(0.1*0.1)
+F5[-2,-2] += 1/(0.1*0.1)
+F6[-2,-2] += 1/(0.1*0.1)
+F7[-2,-2] += 1/(0.1*0.1)
+F[-1,-1] += 1/(0.05*0.05)
+F2[-1,-1] += 1/(0.05*0.05)
+F3[-1,-1] += 1/(0.05*0.05)
+F4[-1,-1] += 1/(0.05*0.05)
+F5[-1,-1] += 1/(0.05*0.05)
+F6[-1,-1] += 1/(0.05*0.05)
+F7[-1,-1] += 1/(0.05*0.05)
+#F[4,4] += 1/(0.0001*0.0001)
+#F2[4,4] += 1/(0.0001*0.0001)
+#F3[4,4] += 1/(0.0001*0.0001)
+#F4[4,4] += 1/(0.0001*0.0001)
+#F5[4,4] += 1/(0.0001*0.0001)
+#F[1,1] += 1/(0.000001*0.000001)
+#F2[1,1] += 1/(0.000001*0.000001)
+#F3[1,1] += 1/(0.000001*0.000001)
+#F4[1,1] += 1/(0.000001*0.000001)
+#F5[1,1] += 1/(0.000001*0.000001)
+#F[0,0] += 1/(0.1*0.1)
+#F2[0,0] += 1/(0.1*0.1)
+#F3[0,0] += 1/(0.1*0.1)
+#F4[0,0] += 1/(0.1*0.1)
+#F5[0,0] += 1/(0.1*0.1)
+
+
+
+
+def plot_fisher(F, basetitle, i,j, color, rescale=False):
+    
+    F_inv = numpy.linalg.inv(F)
+
+    ixgrid = numpy.ix_([j,i],[j,i])
+
+    subM = F_inv[ixgrid]
+
+    print subM
+    
+    sigma1 = numpy.sqrt(subM[0,0])
+    sigma2 = numpy.sqrt(subM[1,1])
+
+    subF = numpy.linalg.inv(subM)
+    
+    l, v = numpy.linalg.eig(subM)
+    l = numpy.sqrt(l)
+    
+    area = l[0]*l[1]*numpy.pi
+    
+    print names[j] + ' sd: ' + str(sigma1)
+    print names[i] + ' sd: ' + str(sigma2)
+    print 'area: ' + str(area)
+    print 'fom: ' + str(1.0/area)
+
+    ax = plt.subplot(111)
+    ax.set_title(basetitle + ': ' + names[j] + ' vs ' + names[i])
+    
+    ax.set_xlabel(names[j])
+    ax.set_ylabel(names[i])
+    
+    center_i = centers[params[i]]
+    center_j = centers[params[j]]
+    
+    ell = Ellipse(xy=(center_j,center_i),width=l[0]*2, height=l[1]*2, angle=numpy.rad2deg(-numpy.arccos(v[0,0])))
+    ell.set_facecolor('none')
+    ell.set_edgecolor(color)
+    ax.add_artist(ell)
+
+    if rescale:
+        plt.xlim([center_j-numpy.sqrt(subM[0,0])*1.2,center_j+numpy.sqrt(subM[0,0])*1.2])
+        plt.ylim([center_i-numpy.sqrt(subM[1,1])*1.2,center_i+numpy.sqrt(subM[1,1])*1.2])
+        plt.scatter([center_j],[center_i])
+
+for i in range(n):
+    for j in range(i):
+
+        if names[i] != 's8':
+            continue
+        if names[j] != 'om':
+            continue
+
+        handles = []
+
+        plot_fisher(F, basetitle, i,j,'blue',True)
+
+        plt.plot([],color='blue',label='PS 256')
+
+        plot_fisher(F2, basetitle, i, j, 'green',False)
+
+        plt.plot([],color='green',label='Moments 256')
+
+        plot_fisher(F3, basetitle, i, j, 'red', False)
+            
+        plt.plot([],color='red',label='PDF 256 + 1600 bins')
+        
+#        plot_fisher(F3,basetitle,i,j, 'black',False)
+#        plt.plot([],color='black',label='PDF 256')
+
+        plot_fisher(F4, basetitle, i, j, 'purple', False)
+        plt.plot([],color='purple',label='log PDF 256 + 800 bins')
+
+#        plot_fisher(F7,basetitle,i,j, 'red', False)
+#        plt.plot([],color='red', label='Moment 1024')
+        
+#        plot_fisher(F6,basetitle,i,j, 'purple',False)
+#        plt.plot([],color='purple',label='Moment 256')
+        
+        plt.legend()
+        
+        #plt.xlim([0.15,0.43])
+        #plt.ylim([0.61,1.03])
+        
+        plt.xlim([0.05,0.53])
+        plt.ylim([0.41,1.23])
+
+
+        #plt.xlim([0.0,1.2])
+        #plt.ylim([0.2,1.5])
+
+        plt.show()
